a5736da1a65076790fc1f8cc48ac7c34f05d9e38
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-02 12:32:18 +0200

    Add basic fuctions. First working version

---
+++ coffe-machine-main.py
@@ -0,0 +1,33 @@
+drink_list = ["Espresso", "Americano", "Americano w Milk", "Late", "Cappuchino", "Flat-White"]
+userInput = None
+
+def print_Drink_List():
+    for id, drink_name in enumerate(drink_list):
+        print(f"[{int(id) + 1}] - {drink_name}\n")
+
+
+def prepare_Drink(drink_id, sugar_amount):
+    print(f"Here is your {drink_list[int(drink_id)-1]} with {sugar_amount} sugar! Enjoy!")
+
+
+def add_Sugar():
+    sugar_qty = int(input("Select sugar, 0-5.\n[0] - None, [5] - Hella lot:\n"))
+    return sugar_qty
+
+
+def main():
+    userInput = int(input("Hello! Would you like a drink?\nInput [1] for YES, [0] for NO:\n"))
+
+    if userInput == 1:
+        print_Drink_List()
+        while userInput:
+            selected_drink = input("Please, select a drink: ")
+            sugar_amount = add_Sugar()
+            prepare_Drink(selected_drink, sugar_amount)
+            break
+    else:
+        pass
+    return 0
+
+
+main()
