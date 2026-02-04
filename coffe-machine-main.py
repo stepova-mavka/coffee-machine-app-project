6614f3a6d41c0c2f89a0cc30447843e86e6999d3
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-04 15:00:59 +0200

    Add data vaidation on machine start and sugar selection

--- coffe-machine-main.py
+++ coffe-machine-main.py
@@ -11,14 +11,31 @@ def prepare_Drink(drink_id, sugar_amount):
 
 
 def add_Sugar():
-    sugar_qty = int(input("Select sugar, 0-5.\n[0] - None, [5] - Hella lot:\n"))
+    while True:
+        try:
+            sugar_qty = int(input("Select sugar, 0 - 5.\n[0] - None, [5] - Hella lot:\n"))
+        except ValueError:
+            print("Please input a number!\n")
+        else:
+            if 5 < sugar_qty or sugar_qty < 0:
+                print("Invalid number! Please input 0 - 5.\n")
+            else:
+                break
     return sugar_qty
 
 
-def main():
-    
-    userInput = int(input("Hello! Would you like a drink?\nInput [1] for YES, [0] for NO:\n"))
+def main(userInput):
 
+    while userInput != 0 and userInput != 1:
+        try:
+            userInput = int(input("Hello! Would you like a drink?\nInput [1] for YES, [0] for NO:\n"))
+        except ValueError:
+            print("Please input a number!\n")
+        else:
+            if userInput != 0 and userInput != 1:
+                print("Invalid input! Please try again.\n")
+
+    
     while userInput:
         print_Drink_List()
         selected_drink = input("Please, select a drink: ")
@@ -34,4 +51,4 @@ def main():
     return print("Have a great day!")
 
 
-main()
+main(userInput)
