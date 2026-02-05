e6e39752335469f5bb2b6d4f83c089a39258029c
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-05 15:10:55 +0200

    Finished data validation

--- coffe-machine-main.py
+++ coffe-machine-main.py
@@ -3,22 +3,37 @@ userInput = None
 
 def print_Drink_List():
     for id, drink_name in enumerate(drink_list):
-        print(f"[{int(id) + 1}] - {drink_name}\n")
+        print(f"\n[{int(id) + 1}] - {drink_name}")
 
 
 def prepare_Drink(drink_id, sugar_amount):
-    print(f"Here is your {drink_list[int(drink_id)-1]} with {sugar_amount} sugar! Enjoy!\n\n")
+    print(f"\nHere is your {drink_list[int(drink_id)-1]} with {sugar_amount} sugar! Enjoy!\n")
+
+
+def select_Drink():
+    while True:
+        try:
+           drink_value = int(input("\nPlease, select a drink:\n"))
+        except ValueError:
+            print("\nPlease input a number!")
+        else:
+            if 1 <= drink_value <= len(drink_list):
+                break  
+            else:
+                print("\nSelect a valid drink number!")
+    return drink_value
+
 
 
 def add_Sugar():
     while True:
         try:
-            sugar_qty = int(input("Select sugar, 0 - 5.\n[0] - None, [5] - Hella lot:\n"))
+            sugar_qty = int(input("\nSelect sugar, 0 - 5.\n\n[0] - None, [5] - Hella lot:\n"))
         except ValueError:
-            print("Please input a number!\n")
+            print("\nPlease input a number!")
         else:
             if 5 < sugar_qty or sugar_qty < 0:
-                print("Invalid number! Please input 0 - 5.\n")
+                print("\nInvalid number! Please input 0 - 5.")
             else:
                 break
     return sugar_qty
@@ -28,27 +43,27 @@ def main(userInput):
 
     while userInput != 0 and userInput != 1:
         try:
-            userInput = int(input("Hello! Would you like a drink?\nInput [1] for YES, [0] for NO:\n"))
+            userInput = int(input("\nHello! Would you like a drink?\n\nInput [1] for YES, [0] for NO:\n"))
         except ValueError:
-            print("Please input a number!\n")
+            print("\nPlease input a number!")
         else:
             if userInput != 0 and userInput != 1:
-                print("Invalid input! Please try again.\n")
+                print("\nInvalid input! Please try again.")
 
     
     while userInput:
         print_Drink_List()
-        selected_drink = input("Please, select a drink: ")
+        selected_drink = select_Drink()
         sugar_amount = add_Sugar()
         prepare_Drink(selected_drink, sugar_amount)
-        userInput = int(input("Would you like another one?\nInput [1] for YES, [0] for NO:\n"))
+        userInput = int(input("\nWould you like another one?\n\nInput [1] for YES, [0] for NO:\n"))
         if userInput == 0:
             break
         else:
             pass
 
 
-    return print("Have a great day!")
+    return print("\nHave a great day!")
 
 
 main(userInput)
