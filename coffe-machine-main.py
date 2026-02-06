3f5935e6d1498c66ee18d8ae1dcafc9f4de87c05
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-06 12:33:52 +0200

    Update naming to PEP8, Add input_message function

--- coffe-machine-main.py
+++ coffe-machine-main.py
@@ -1,16 +1,30 @@
-drink_list = ["Espresso", "Americano", "Americano w Milk", "Late", "Cappuchino", "Flat-White"]
-userInput = None
+drink_list = ["Espresso", "Americano", "Americano w Milk",
+               "Latte", "Cappuccino", "Flat-White"]
 
-def print_Drink_List():
+user_input = None
+
+def input_message(run_flag):
+    str_greeting = "\nHello! Would you like a drink?\n"
+    str_another_drink = "\nWould you like another one?\n"
+    str_input_guidelines = "\nInput [1] for YES, [0] for NO:\n"
+    if run_flag:
+        input_message = int(input(f"{str_greeting} {str_input_guidelines}"))
+    else:
+        input_message = int(input(f"{str_another_drink} {str_input_guidelines}"))
+    return input_message
+
+
+def print_drink_list() -> None:
     for id, drink_name in enumerate(drink_list):
         print(f"\n[{int(id) + 1}] - {drink_name}")
 
 
-def prepare_Drink(drink_id, sugar_amount):
-    print(f"\nHere is your {drink_list[int(drink_id)-1]} with {sugar_amount} sugar! Enjoy!\n")
+def prepare_drink(drink_id, sugar_amount):
+    print(f"\nHere is your {drink_list[int(drink_id)-1]} " \
+          f"with {sugar_amount} sugar! Enjoy!\n")
 
 
-def select_Drink():
+def select_drink():
     while True:
         try:
            drink_value = int(input("\nPlease, select a drink:\n"))
@@ -25,10 +39,11 @@ def select_Drink():
 
 
 
-def add_Sugar():
+def add_sugar():
     while True:
         try:
-            sugar_qty = int(input("\nSelect sugar, 0 - 5.\n\n[0] - None, [5] - Hella lot:\n"))
+            sugar_qty = int(input("\nSelect sugar, 0 - 5.\n\n" \
+                                  "[0] - None, [5] - Hella lot:\n"))
         except ValueError:
             print("\nPlease input a number!")
         else:
@@ -39,31 +54,33 @@ def add_Sugar():
     return sugar_qty
 
 
-def main(userInput):
+def main():
+    first_run = True
 
-    while userInput != 0 and userInput != 1:
+    while True:
         try:
-            userInput = int(input("\nHello! Would you like a drink?\n\nInput [1] for YES, [0] for NO:\n"))
+            user_input = input_message(first_run)
         except ValueError:
             print("\nPlease input a number!")
         else:
-            if userInput != 0 and userInput != 1:
+            if user_input != 0 and user_input != 1:
                 print("\nInvalid input! Please try again.")
+            else:
+                first_run = False
+                break
 
     
-    while userInput:
-        print_Drink_List()
-        selected_drink = select_Drink()
-        sugar_amount = add_Sugar()
-        prepare_Drink(selected_drink, sugar_amount)
-        userInput = int(input("\nWould you like another one?\n\nInput [1] for YES, [0] for NO:\n"))
-        if userInput == 0:
+    while user_input:
+        print_drink_list()
+        selected_drink = select_drink()
+        sugar_amount = add_sugar()
+        prepare_drink(selected_drink, sugar_amount)
+        user_input = input_message(first_run)
+        if user_input == 0:
             break
-        else:
-            pass
 
 
     return print("\nHave a great day!")
 
 
-main(userInput)
+main()
